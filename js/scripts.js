ee3448a238fe658e2cedb318cac5fecd7f4156c6
Johannes164 <94741998+Johannes164@users.noreply.github.com>
2022-10-23 20:11:36 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+function barAnimasjonFunksjon(x) {
+    x.classList.toggle("change"); /* element.classList.toggle skrur .change klassen på for x, altså i barAnimasjonFunksjon(x). Her defineres funksjonen, der den er brukt er "x" variabelen (this), som betyr at den gjelder det som ble trykket på for onclick, altså klassen change blir skrudd på for burgerbarcontainer */
+}
