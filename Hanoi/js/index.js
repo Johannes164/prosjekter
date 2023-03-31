65d2f6e4c565b812df42980b1d8eb6a20beab6dd
Johannes164 <94741998+Johannes164@users.noreply.github.com>
2023-03-31 19:13:26 +0200

    Add files via upload

--- Hanoi/js/index.js
+++ Hanoi/js/index.js
@@ -3,6 +3,8 @@
 let selectedRod     = null;                                         //variabel som holder styr på hvilken stolpe som er valgt, null betyr at ingen er valgt
 let solveDelay      = 100;                                          //delay mellom flytting av skive, i millisekunder
 let solving         = false;                                        //variabel som holder styr på om tårnet holder på å løses
+let diskInputMinValue = 0;                                          //minste antall skiver som kan bygges opp
+let diskInputMaxValue = 15;                                         //maks antall skiver som kan bygges opp
 
     //selektorer (DOM-noder/elementer)
 let tooltip         = document.querySelector("#tooltip");           //tooltipen (containeren for all tooltip-innhold)
@@ -93,10 +95,10 @@ function moveDisk2(rodFrom, rodTo) {                             //Flyttefunksjo
 }
 
 function checkInput() {                                         //funksjon som viser feilmelding popup viss input er ugyldig
-    if (isNaN(diskInput.value) || diskInput.value < 0 || diskInput.value > 9) { //sjekker om input er et tall mellom 0 og 9
+    if (isNaN(diskInput.value) || diskInput.value < diskInputMinValue || diskInput.value > diskInputMaxValue) { //sjekker om input er et tall mellom diskInputMinValue og diskInputMaxValue
         let errorMessage = document.createElement("span"); //lager en span med klasse "error-message" og tekst
         errorMessage.classList.add("error-message");
-        errorMessage.innerHTML = "Ugyldig input <br> skriv et tall fra 0 til 9 <br>";
+        errorMessage.innerHTML = "Ugyldig input <br> skriv et tall fra " + diskInputMinValue + " til " + diskInputMaxValue + " <br>";
     
         let count = 5; //teller ned fra count til 0
         let countdown = document.createElement("span"); //lager en span med id "countdown" og tekst som oppdateres hvert sekund
@@ -104,7 +106,7 @@ function checkInput() {                                         //funksjon som v
         countdown.innerHTML = count;
         errorMessage.appendChild(countdown); //legger til countdown i errorMessage
         
-        diskInput.parentElement.appendChild(errorMessage); //legger errorMessage til på siden
+        document.body.appendChild(errorMessage); //legger errorMessage til på siden
         
         // Oppdaterer nedtellingen hvert sekund (countdown)
         let countdownInterval = setInterval(function() { //setInterval gjentar en funksjon hvert x millisekund, her gjentas den til count er 0
@@ -133,15 +135,15 @@ function resetDisks() {                                         //funksjon som r
     }
     buildDisks(diskInput.value);
     checkInput(); //resetDisks funksjonen kalles også ved input, så vi sjekker om input er gyldig
-    solveButton.addEventListener("click", solveButtonSolve); //legger til eventlisteneren for solve knappen i tilfelle den er fjernet av solve knappen (kode rett under)
+    solveButton.addEventListener("click", solveDisks); //legger til eventlisteneren for solve knappen i tilfelle den er fjernet av solve knappen (kode rett under)
     solveButton.innerHTML = "Solve"; //endrer teksten på knappen tilbake til "Solve"
 }
 
-function solveButtonSolve() {                                //funksjon som løser tårnet når brukeren trykker på "solve" knappen (brukes til solve knapp)
+function solveDisks() {                                //funksjon som løser tårnet når brukeren trykker på "solve" knappen (brukes til solve knapp)
     resetDisks();
     solveH(rods[0].dataset.disks, rods[0], rods[2], rods[1]);
     //fjern funksjonen for å legge den til igjen når den er ferdig
-    solveButton.removeEventListener("click", solveButtonSolve); //fjerner eventlisteneren for å hindre at brukeren trykker på knappen flere ganger, den legges til igjen når solveH er ferdig
+    solveButton.removeEventListener("click", solveDisks); //fjerner eventlisteneren for å hindre at brukeren trykker på knappen flere ganger, den legges til igjen når solveH er ferdig
     solveButton.innerHTML = "Solving..."; //endrer teksten på knappen til "Solving..."
     solving = true; //setter solving til true sånn at reset kan stoppe solveH
 }
@@ -165,7 +167,7 @@ async function solveH(n, from, to, extra) {                     //rekursiv funks
         await solveH(n - 1, extra, to, from); 
 
         if (rods[2].dataset.disks === n) { //viss tredje stolpe har n skiver, er tårnet løst, så vi gir solveButton tilbake eventlistener
-            solveButton.addEventListener("click", solveButtonSolve);
+            solveButton.addEventListener("click", solveDisks);
             solveButton.innerHTML = "Solve";
             solving = false;
         }
@@ -187,10 +189,11 @@ function selectrod(rod) {                                       //funksjon som l
     if (solving === true || (!rod.lastChild && selectedRod == null)) { //viss tårnet holder på å løses, eller viss det er første gang du trykker samt at du trykker på en tom stolpe returner vi tidlig for å hindre feil
         return;
     }
-    if (selectedRod === rod) { //viss du trykker på samme stolpe
+    if (selectedRod === rod) { //viss du trykker på samme stolpe (rod parameteren settes alltid til stolpen du trykker på (se på eventlisteneren))
         selectedRod.dataset.selected = "false"; //setter selected til false
         selectedRod.lastChild.classList.remove("selected-disk"); //fjerner klassen selected-disk fra den øverste skiven (styling som viser at den er valgt)
         selectedRod = null; //setter selectedRod til null
+        clickableRods(); //oppdaterer hvilkne stolper som er clickable
     }
     else if (selectedRod === null) { //viss du trykker på en stolpe uten å ha valgt en annen først
         rod.dataset.selected = "true"; //setter selected til true
@@ -198,16 +201,20 @@ function selectrod(rod) {                                       //funksjon som l
         rod.lastChild.classList.add("selected-disk");  //legger til klassen selected-disk på den øverste skiven (styling som viser at den er valgt)
         for (let i = 0; i < rods.length; i++) { // gir alle stolpene clickable (for å vise at de kan flyttes til, og at den som allerede er valgt kan deselectes)
             rods[i].classList.add("clickable");
+            //fjerner clickable dersom stolpens øverste skive er mindre enn den valgte skiven
+            if (rods[i].lastChild && parseInt(rods[i].lastChild.style.width) < parseInt(rod.lastChild.style.width)) {
+                rods[i].classList.remove("clickable");
+            }
         }
     }
     else { //viss du trykker på en stolpe etter å ha valgt en annen først
         let selectedDisk = selectedRod.lastChild; //henter ut siste barnet til rod, som er den øverste skiven, husk at selectedRod er den første valgte, siden selectedRod ikke har blitt oppdatert enda
-        if (rod.lastChild === null || parseInt(selectedDisk.id.slice(4)) > parseInt(rod.lastChild.id.slice(4))) {  //sjekker om trekket er lovlig, viss det er lovlig flyttes skiven og stolpene som kan klikkes får clickable
+        if (rod.lastChild === null || parseInt(selectedDisk.style.width) < parseInt(rod.lastChild.style.width)) {  //sjekker om trekket er lovlig, viss det er lovlig flyttes skiven og stolpene som kan klikkes får clickable
             selectedRod.lastChild.classList.remove("selected-disk");
             moveDisk(selectedDisk, rod);
-            clickableRods();
             selectedRod.dataset.selected = "false";
             selectedRod = null;
+            clickableRods(); //oppdaterer hvilkne stolper som er clickable
          }
     }
 }
@@ -219,7 +226,7 @@ function selectrod(rod) {                                       //funksjon som l
 resetButton     .addEventListener("click", resetDisks       );  //legger til eventlistener på reset knappen             som kjører resetDisks       når den blir trykket på
 diskInput       .addEventListener("input", resetDisks       );  //legger til eventlistener på diskInput feltet          som kjører resetDisks       når den blir endret
 solveDelayInput .addEventListener("input", updateSolveDelay );  //legger til eventlistener på solveDelayInput feltet    som kjører updateSolveDelay når den blir endret
-solveButton     .addEventListener("click", solveButtonSolve );  //legger til eventlistener på solve knappen             som kjører solveButtonSolve når den blir trykket på
+solveButton     .addEventListener("click", solveDisks       );  //legger til eventlistener på solve knappen             som kjører solveDisks når den blir trykket på
 
 for (let i = 0; i < rods.length; i++) {                         //Legger til eventlisteners til rods med løkke (kan ikke bruke for of fordi det teknisk sett ikke er et array men en NodeList)
     rods[i].addEventListener("click", function() { selectrod(rods[i]) }); //selectrod er inni en funksjon for å kunne bruke argument i den, her betyr det at rod parameteren settes til stolpen som klikkes
